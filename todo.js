481afdb3d9df88bae6e60a4504104744d6a29e16
Divya Singh <65713449+divya-giiter@users.noreply.github.com>
2021-08-31 11:09:12 +0530

    Add files via upload

---
+++ todo.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+	$("ul").on("click", "li", function () {
+		console.log("clicked");
+	    $(this).toggleClass("done");
+	});
+
+	$("ul").on("click", "span", function(event){
+		$(this).parent().fadeOut(200, function(){
+			$(this).remove();
+		});		
+		event.stopPropagation();
+	});
+
+	$("input[type='text']").keypress(function(event){
+		if(event.which === 13){
+		 	var newTodo = $(this).val();
+			$(this).val("");
+			$("ul").append("<li><span><i class='fas fa-trash'></i></span> " + newTodo + "</li>");
+		}
+	});
+	
+	$(".fa-plus").on("click", function(){
+		$("input[type='text']").fadeToggle();
+	});
+
+});
+
